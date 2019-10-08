9d3c2f36d7b7f8c5d0fc043606bf03723a401a84
Huda Yousif <hjdoski@gmail.com>
2019-10-08 22:03:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,8 @@
 
 
-def unsafe?(speed)
-  if speed > 40 
-    return true
-  else speed < 60
-    return true
-  end 
+def unsafe? speed
+  speed < 40 || speed > 60
 end
-
-
 
 def not_safe?(speed)
   speed < 40 || speed > 60 ? true : false
